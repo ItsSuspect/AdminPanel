e9477703589fb91f6adccfc9f1f5994ee840ac81
iQworte <95915893+iQworte@users.noreply.github.com>
2024-08-31 20:36:56 +0900

    1.0.5
    Мелкие правки
    Удалена таблица "Авторизаций"

--- FrostGuru/static/js/partner.js
+++ FrostGuru/static/js/partner.js
@@ -1,55 +1,47 @@
 function openDetailingPartnerWindow() {
-    document.getElementById('detailing-popup').style.display = 'block';
-    document.querySelector('.overlay').style.display = 'block';
+    document.getElementById('detailing-popup').style.display = 'block'
+    document.querySelector('.overlay').style.display = 'block'
 }
 
 function closePopup() {
     document.querySelectorAll('.popup').forEach(popup => {
-        popup.style.display = 'none';
-    });
-    document.querySelector('.overlay').style.display = 'none';
+        popup.style.display = 'none'
+    })
+    document.querySelector('.overlay').style.display = 'none'
 }
 
-document.querySelector('.overlay').addEventListener('click', function(event) {
-    if (event.target === this) {
-        closePopup();
-    }
-});
+document.querySelector('.overlay').addEventListener('click', (event)=> {
+    if (event.target !== this) return
+    closePopup()
+})
 
-function calculate() {
+async function calculate() {
     const startDateInput = document.getElementById('start-date').value;
     const endDateInput = document.getElementById('end-date').value;
 
-    const startDateTimestamp = Math.floor(new Date(startDateInput).getTime() / 1000);
-    const endDateTimestamp = Math.floor(new Date(endDateInput).getTime() / 1000);
+    const startDateTimestamp = Math.floor(new Date(startDateInput).getTime() / 1000)
+    const endDateTimestamp = Math.floor(new Date(endDateInput).getTime() / 1000)
 
-    const CalculateRequest = {
+    const urlParams = new URLSearchParams(window.location.search)
+    const key = urlParams.get('key')
+
+    let body = {
         startDate: startDateTimestamp,
         endDate: endDateTimestamp
-    };
+    }
 
-    const urlParams = new URLSearchParams(window.location.search);
-    const key = urlParams.get('key');
-
-    fetch('/partner/calculate?key=' + key, {
+    let response = await fetch('/partner/calculate?key=' + key, {
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(CalculateRequest)
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(body)
     })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            document.querySelector('.detailing-popup__income-detail').style.display = 'flex';
-            document.getElementById('totalIncome').textContent = data.sumIncome.toFixed(2) + ' $';
-            document.getElementById('totalAvailable').textContent = data.incomeAvailable.toFixed(2) + ' $';
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
+
+    if (!response.ok) {
+        throw new Error('Network response was not ok')
+    } else {
+        let data = await response.json()
+        document.querySelector('.detailing-popup__income-detail').style.display = 'flex';
+        document.getElementById('totalIncome').textContent = data.sumIncome.toFixed(2) + ' $';
+        document.getElementById('totalAvailable').textContent = data.incomeAvailable.toFixed(2) + ' $';
+    }
 }
